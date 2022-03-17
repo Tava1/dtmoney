a8dcbabff9b024bfe0f096d93e9b6a7b1f112dea
Gustavo Santos <gusta99sant@gmail.com>
2022-03-17 20:36:04 -0300

    Contexts and Hooks: Create Hook

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,5 +1,5 @@
-import { createContext, ReactNode, useEffect, useState } from 'react';
-import { api } from './services';
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
+import { api } from '../services';
 
 interface Transaction {
   id: number;
@@ -32,7 +32,7 @@ interface TransactionsContextData {
   createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
-export const TransactionsContext = createContext<TransactionsContextData>(
+const TransactionsContext = createContext<TransactionsContextData>(
   {} as TransactionsContextData
 );
 
@@ -59,4 +59,10 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       {children}
     </TransactionsContext.Provider>
   )
+}
+
+export function useTransactions() {
+  const context = useContext(TransactionsContext);
+
+  return context;
 }
