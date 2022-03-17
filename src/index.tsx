f088f19728baa851c27781fe54b98e553fd6d9ac
Gustavo Santos <gusta99sant@gmail.com>
2022-03-17 20:28:05 -0300

    Contexts and Hooks: Calculate summary

--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,7 @@ createServer({
           title: 'Salário',
           type: 'deposit',
           category: 'Desenvolvimento',
-          amount: '12000',
+          amount: 12000,
           createdAt: new Date('2021-02-12 09:00:00'),
         },
         {
@@ -25,7 +25,7 @@ createServer({
           title: 'Aluguel',
           type: 'withdraw',
           category: 'Casa',
-          amount: '1100',
+          amount: 1100,
           createdAt: new Date('2021-02-14 11:00:00'),
         }
       ]
