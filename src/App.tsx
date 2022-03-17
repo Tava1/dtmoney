83204b3c6946f0c1bae0ff04c46a10fa9927bfba
Gustavo Santos <gusta99sant@gmail.com>
2022-03-17 19:31:40 -0300

    Contexts and Hooks: Transactions with Context API

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Header } from './components/Header';
 import Modal from 'react-modal';
 import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from './styles/global';
-import { TransactionsContext } from './TransactionsContext';
+import { TransactionsProvider } from './TransactionsContext';
 
 Modal.setAppElement('#root');
 
@@ -19,7 +19,7 @@ export function App() {
     setIsNewTransactionModalOpen(false);
   }
   return (
-    <TransactionsContext.Provider value={[]}>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
       <Dashboard />
@@ -30,6 +30,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </TransactionsContext.Provider>
+    </TransactionsProvider>
   );
 }
